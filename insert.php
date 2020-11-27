ddbce4cf77e519ada03222abff706ee4214673be
Piradon-lab <71110711+Piradon-lab@users.noreply.github.com>
2020-11-27 10:56:08 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -22,7 +22,7 @@
     $Product = $_POST['Product'];
     $Price = $_POST['Price'];
     $Amount = $_POST['Amount'];
-    $Total = $_POST[$Amount*$Price];
+    $Total = $_POST[$_POST['Price']*$_POST['Amount']];
 
 
     $sql = "INSERT INTO sinka (Product , Price , Amount, Total) VALUES ('$Product', '$Price', '$Amount', '$Total')";
